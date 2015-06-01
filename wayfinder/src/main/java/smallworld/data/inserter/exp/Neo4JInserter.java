29c7c0c38414908981b8d11106c3ab69c9012a4d
chungweihang <chungwei.hang@gmail.com>
2015-05-31 23:36:10 -0400

    New Neo4j schema: circle as node

---
+++ wayfinder/src/main/java/smallworld/data/inserter/exp/Neo4JInserter.java
@@ -0,0 +1,151 @@
+package smallworld.data.inserter.exp;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.kernel.impl.util.FileUtils;
+import org.neo4j.unsafe.batchinsert.BatchInserter;
+import org.neo4j.unsafe.batchinsert.BatchInserters;
+
+import smallworld.data.RelationshipTypes;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+public class Neo4JInserter {
+	
+	private static final Logger logger = LogManager.getLogger();
+	static final String CIRCLE_NAME = "circleName";
+	
+	final BatchInserter inserter;
+	boolean isDirected = false;
+	final Multimap<Long, Long> friendEdges;
+	final Multimap<Long, Long> circleEdges;
+	final Map<Object, Long> nodeToIds;
+	final Map<String, Long> circleToIds;
+	
+	// statistics
+	int maxCircle = 0; // need to set explicitly
+	long totalCircleSize = 0;
+	
+	public Neo4JInserter(String path) {
+		// config
+		Map<String, String> config = new HashMap<>();
+        config.put("neostore.nodestore.db.mapped_memory", "90M");
+        config.put("neostore.relationshipstore.db.mapped_memory", "3G");
+        config.put("neostore.propertystore.db.mapped_memory", "50M");
+        config.put("neostore.propertystore.db.strings.mapped_memory", "100M");
+        config.put("neostore.propertystore.db.arrays.mapped_memory", "0M");
+        inserter = BatchInserters.inserter(path, config);
+        
+        friendEdges = HashMultimap.create();
+        circleEdges = HashMultimap.create();
+        nodeToIds = new HashMap<>();
+        circleToIds = new HashMap<>();
+   }
+	
+	void insert() throws IOException {
+		delete(inserter.getStoreDir());
+		inserter.shutdown();
+		 
+		logger.info("Number of nodes: " + nodeToIds.size());
+		logger.info("Number of friendships: " + friendEdges.size());
+		logger.info("Number of circles: " + circleToIds.size());
+		logger.info("Max size of circle: " + maxCircle);
+		logger.info("Average circle size: " + ((double) totalCircleSize) / circleToIds.size());
+		logger.info("Average membership size: " + ((double) totalCircleSize) / nodeToIds.size());
+		
+	}
+	
+	public void addCircle(String circleName) {
+		if (!circleToIds.containsKey(circleName)) {
+			long id = inserter.createNode(null);
+			circleToIds.put(circleName, id);
+		} 
+	}
+	
+	public void setCircle(String circleName, Object node) {
+		if (!nodeToIds.containsKey(node)) {
+			throw new IllegalArgumentException("Node: " + node + " does not exist!"); 
+		}
+		
+		if (circleToIds.containsKey(circleName)) {
+			// always from node to circle
+			if (addRelationship(nodeToIds.get(node), circleToIds.get(circleName), RelationshipTypes.CIRCLE.type())) {
+				circleEdges.put(nodeToIds.get(node), circleToIds.get(circleName));
+				totalCircleSize++;
+			}
+		} else {
+			throw new IllegalArgumentException("Cirlce: " + circleName + " does not exist!"); 
+		}
+	}
+	
+	public void addNode(Object node) {
+		addNode(node, emptyMap());
+	}
+	
+	public void addNode(Object node, Map<String, Object> features) {
+		if (!nodeToIds.containsKey(node)) {
+			long id = inserter.createNode(features);
+			nodeToIds.put(node, id);
+		} else {
+			long id = nodeToIds.get(node);
+			features.putAll(inserter.getNodeProperties(id));
+			inserter.setNodeProperties(id, features);
+		}
+	}
+	
+	private static Map<String, Object> emptyMap() {
+		return new HashMap<>();
+	}
+	
+	private boolean addRelationship(long fromNodeId, long toNodeId, RelationshipType type) {
+		if (!relationshipExists(type, fromNodeId, toNodeId)) {
+			// relationship does not exist, create new relationship
+			inserter.createRelationship(fromNodeId, toNodeId, type, null);
+			return true;
+		} else {
+			//logger.warn("Relationship " + fromNodeId + " => " + toNodeId + " exists!");
+			return false;
+		}
+	}
+	
+	public void addFriend(Object fromNode, Object toNode) {
+		if (!nodeToIds.containsKey(fromNode)) {
+			throw new IllegalArgumentException("Node: " + fromNode + " does not exist!"); 
+		}
+		
+		if (!nodeToIds.containsKey(toNode)) {
+			throw new IllegalArgumentException("Node: " + toNode + " does not exist!"); 
+		}
+		if (addRelationship(nodeToIds.get(fromNode), nodeToIds.get(toNode), RelationshipTypes.FRIEND.type())) {
+			friendEdges.put(nodeToIds.get(fromNode), nodeToIds.get(toNode));
+		}
+	}
+	
+	private boolean relationshipExists(RelationshipType type, long fromNodeId, long toNodeId) {
+		if (type == RelationshipTypes.FRIEND.type()) {
+			if (isDirected) {
+				// directed
+				return friendEdges.containsEntry(fromNodeId, toNodeId);
+			} else {
+				// undirected
+				return friendEdges.containsEntry(fromNodeId, toNodeId) || friendEdges.containsEntry(toNodeId, fromNodeId);
+			}
+		} else if (type == RelationshipTypes.CIRCLE.type()) {
+			return circleEdges.containsEntry(fromNodeId, toNodeId);
+		} else {
+			throw new IllegalArgumentException("Unknown relation type: " + type.name());
+		}
+		
+	}
+	
+	private void delete(String path) throws IOException {
+		FileUtils.deleteRecursively(new File(path));
+	}
+}
---
+++ wayfinder/src/main/java/smallworld/data/inserter/exp/SocialCircleBatchInsert.java
@@ -0,0 +1,251 @@
+package smallworld.data.inserter.exp;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import smallworld.data.RelationshipTypes;
+
+/**
+ * Batch insert data to neo4j
+ * 
+ * @author chang
+ *
+ */
+public class SocialCircleBatchInsert {
+	
+	private static final Logger logger = LogManager.getLogger();
+
+	final Neo4JInserter inserter;
+	
+	protected String neo4jPath, dataPath;
+	
+	public SocialCircleBatchInsert(String neo4jPath, String dataPath) {
+		inserter = new Neo4JInserter(neo4jPath);
+		this.dataPath = dataPath;
+	}
+	
+	/**
+	 * Utility method to load a file into a list of lines.
+	 * 
+	 * @param filename
+	 * @return
+	 */
+	private List<String> readLine(String filename) {
+		return readLine(new File(filename));
+	}
+	
+	/**
+	 * Utility method to load a file into a list of lines.
+	 * 
+	 * @param filename
+	 * @return
+	 */
+	private List<String> readLine(File f) {
+		List<String> lines = new ArrayList<>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(f))) {
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				lines.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return lines;
+	}
+	
+	/*
+	 * Add ego features as properties of a node
+	 */
+	private Map<String, Object> readEgoFeatures(String filename, List<String> features) {
+		Map<String, Object> properties = new HashMap<>();
+		
+		List<String> lines = readLine(filename);
+		for (String line : lines) {
+			line = line.trim();
+			
+			if (line.length() == 0) continue;
+			
+			String[] tokens = line.split(" ");
+			
+			for (int i = 0; i < tokens.length; i++) {
+				int bit = Integer.parseInt(tokens[i]);
+				
+				if (1 == bit) {
+					properties.put(features.get(i), "");
+				}
+			}
+		}
+		
+		return properties;
+	}
+	
+	/**
+	 * Read feature names from ego.featname
+	 * 
+	 * @param filename
+	 * @return
+	 */
+	private List<String> readFeatures(String filename) {
+		List<String> features = new ArrayList<>();
+		
+		List<String> lines = readLine(filename);
+		for (String line : lines) {
+			int index = line.indexOf(" ");
+			features.add(line.substring(index));
+		}
+		
+		return features;
+	}
+	
+	// all insert operations are here
+	public void insert() throws IOException {
+		
+		File[] files = null;
+        
+        // insert
+        File dir = new File(dataPath);
+		
+		files = dir.listFiles(new FileFilter() {
+		    public boolean accept(File file) {
+		        if (file.getName().endsWith(".edges")) {
+		            return true;
+		        }
+		        return false;
+		    }	
+		});
+		
+		for (File f : files) {
+			
+			String egoString = f.getAbsolutePath().substring(0, f.getAbsolutePath().indexOf("."));
+			logger.info("Processing " + egoString);
+			
+			// Parse ego ID
+			String ego = f.getName().substring(0, f.getName().indexOf("."));
+			
+			// Get feature names
+			List<String> features = readFeatures(egoString + ".featnames");
+
+			// Set ego features
+			Map<String, Object> properties = readEgoFeatures(egoString + ".egofeat", features);
+			// Insert ego
+			inserter.addNode(ego, properties);
+			
+			
+			// ==== EDGES (FRIEND) ====
+			List<String> edges = readLine(f);
+			
+			for (String line : edges) {
+				String[] tokens = line.split(" ");
+				
+				String src = tokens[0];
+				String det = tokens[1];
+				
+				inserter.addNode(src);
+				inserter.addNode(det);
+				
+				// ego is assumed to connect to both src and det
+				inserter.addFriend(ego, src);
+				inserter.addFriend(ego, det);
+				inserter.addFriend(src, det);
+			}
+			
+			// ==== FEAT (KNOWS) ====
+			List<String> feats = readLine(egoString + ".feat");
+			
+			for (String line : feats) {
+				// target-id feat1 feat2 feat3 ...
+				String[] tokens = line.split(" ");
+				
+				Map<String, Object> props = new HashMap<>();
+				String target = tokens[0];
+				
+				for (int i = 1; i < tokens.length; i++) {
+					int bit = Integer.parseInt(tokens[i]);
+					
+					if (1 == bit) {
+						props.put(features.get(i-1), "");
+					}
+				}
+				
+				inserter.addNode(target, props);
+				inserter.addFriend(ego, target);
+			}
+			
+			// Consider each circle as a label
+			List<String> circles = readLine(egoString + ".circles");
+			for (String line : circles) {
+				// circle ego1 ego2 ...
+				String[] tokens = line.split("\t");
+				
+				// Size of circle: ego + all the nodes in tokens = 1 + tokens.length - 1 = tokens.length
+				int circleSize = tokens.length;
+				if (circleSize > inserter.maxCircle) inserter.maxCircle = circleSize;
+				
+				String circleName = ego + ":" + tokens[0];
+				inserter.addCircle(circleName);
+				inserter.setCircle(circleName, ego);
+				
+				for (int i = 1; i < tokens.length; i++) {
+					String target = tokens[i];
+					
+					inserter.addNode(target);
+					inserter.addFriend(ego, target);
+					inserter.setCircle(circleName, target);
+				}
+			}
+		}
+        
+		inserter.insert();
+   }
+	
+	public static void usage() {
+		System.err.println(new StringBuilder()
+			.append("BatchInsert faceboook|gplus|twitter").toString());
+	}
+	
+	private void setDirected(boolean isDirected) {
+		inserter.isDirected = isDirected;
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		if (args.length != 1) {
+			usage();
+			System.exit(0);
+		}
+		
+		String dataset = args[0];
+		
+		SocialCircleBatchInsert insert = new SocialCircleBatchInsert("neo4j/" + dataset + "-exp", "data/" + dataset);
+		
+		if (dataset.equals("facebook")) {
+			insert.setDirected(false);
+		} else if (dataset.equals("gplus")) {
+			insert.setDirected(true); 
+		} else if (dataset.equals("twitter")) {
+			insert.setDirected(true); 
+		} else {
+			usage();
+			System.exit(0);
+		}
+		
+		insert.insert();
+	}
+
+}
